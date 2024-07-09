489a8c63cc57b6093ecc529114abbff46e607495
iftesam15 <143783957+iftesam15@users.noreply.github.com>
2024-07-09 17:39:39 +0600

    Update enrollmentsController.js

--- controllers/enrollmentsController.js
+++ controllers/enrollmentsController.js
@@ -13,37 +13,32 @@ const pool = new Pool({
 const getEnrollmentDetails = async (req, res) => {
   const query = `
     SELECT 
-      
-      e.enrollment_date,
-     
-      CONCAT(s.first_name, ' ', s.last_name) AS student_full_name,
-      c.course_name,
-      CONCAT(i.first_name,' ', i.last_name) AS instructor_full_name
-     
-    FROM 
-      enrollments e
-    JOIN 
-      students s ON e.student_id = s.student_id
-    JOIN 
-      courses c ON e.course_id = c.course_id
-    JOIN 
-      course_instructors ci ON c.course_id = ci.course_id
-    JOIN 
-      instructors i ON ci.instructor_id = i.instructor_id
-    ORDER BY 
-      e.enrollment_date DESC;
+  CONCAT(students.first_name , ' ' ,students.last_name) AS student_full_name,
+  enrollments.enrollment_id AS enrollment_id,
+  courses.course_name,
+  instructors.first_name AS instructor_first_name,
+  instructors.last_name AS instructor_last_name,
+  enrollments.enrollment_date
+FROM
+  enrollments
+  JOIN students ON enrollments.student_id = students.student_id
+  JOIN courses ON enrollments.course_id = courses.course_id
+  JOIN instructors ON enrollments.instructor_id = instructors.instructor_id
+ORDER BY enrollments.enrollment_date DESC;
+
   `;
 
   try {
     const result = await pool.query(query);
-    const enrollements = result.rows.map((row)=>{
+    const enrollements = result.rows.map((row) => {
       return {
+        enrollment_id: row.enrollment_id,
         enrollment_date: row.enrollment_date,
         student_full_name: row.student_full_name,
-        course_name: row.course_name
+        course_name: row.course_name,
         // instructor_full_name: row.instructor_full_name,
-      }
-    })
+      };
+    });
     res.status(200).json({
       status: "success",
       data: enrollements,
